f28a386cd7ea591f522d826e3ae214096939c176
Augusto Spinelli <4614597+gutospinelli@users.noreply.github.com>
2020-01-02 11:53:33 -0300

    batch convert docs to pdf
    Script that uses MsWord as a com object to batch convert to PDF all documents (doc and docx) from current folder

---
+++ DocsToPdf.ps1
@@ -0,0 +1,19 @@
+﻿# the path where your documents live (no recursive)
+$documents_path = 'C:\tmp\es'
+
+# the MsWord application as a com object used to convert docs to pdf
+$msword = New-Object -ComObject Word.Application
+
+# finds .doc and .docx (doc?) and performs pdf conversion using MsWord's SaveAs functionality
+Get-ChildItem -Path $documents_path -Filter *.doc? | ForEach-Object {
+
+    $document = $msword.Documents.Open($_.FullName)
+
+    $pdf_filename = "$($_.DirectoryName)\$($_.BaseName).pdf"
+
+    $document.SaveAs([ref] $pdf_filename, [ref] 17)
+
+    $document.Close()
+}
+# closes the MsWord app
+$msword.Quit()
