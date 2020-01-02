04b7d898ce6ca34776ae8d1b8f6e87672baf3c83
Augusto Spinelli <4614597+gutospinelli@users.noreply.github.com>
2020-01-02 11:38:48 -0300

    Performs a git command from the current dir
    Performs a git command (PULL by default) in all subdirectories (without recursion) from the current dir

---
+++ PullAllRepos.ps1
@@ -0,0 +1,35 @@
+﻿param (
+
+    # The command you want to perform
+    $cmd = "pull"
+)
+
+function Go () {
+
+    # Finds all folders by current path  
+    $gitFolders = dir -Directory
+
+    ForEach ($gitFolder in $gitFolders) {
+
+        # Gets the FullName to perform the desired command 
+        $folder = $gitFolder.FullName 
+
+        Write-Host "Performing git $cmd in folder: '$folder'..." -foregroundColor "green"
+
+        # Go into the folder
+        Push-Location $folder 
+
+        # Perform the command within the folder
+        & git $cmd
+
+        # Go back to the original folder
+        Pop-Location
+    }
+}
+
+function Main () {  
+    Go   
+}
+
+# Executes the main function
+Main  
