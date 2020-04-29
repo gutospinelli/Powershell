53cb8b3fd1b078e18986bcdc0635167e8644132a
Augusto Spinelli <4614597+gutospinelli@users.noreply.github.com>
2020-04-29 18:12:45 -0300

    Starting a script to download pdf files from txt line by line

---
+++ DownloadFilesFromTXT.ps1
@@ -0,0 +1,20 @@
+﻿
+foreach($line in [System.IO.File]::ReadLines("C:\tmp\es\PDFsFinal.txt"))
+{
+      
+    $start_time = Get-Date
+        
+    $Filename = [System.IO.Path]::GetFileName($line)
+    $dest = "C:\tmp\es\books\$Filename"
+
+    $Filename 
+    $dest
+
+    $wc = New-Object System.Net.WebClient
+    $wc.DownloadFile($source, $dest)
+
+	Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
+}
+
+
+
