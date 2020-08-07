a9c4811d1b108200e2f3ca4d5178c0920113e899
Augusto Spinelli <4614597+gutospinelli@users.noreply.github.com>
2020-08-07 14:00:50 -0300

    Docker Aliases
    Alias to run ABC importer and run Elastic Container on Docker

---
+++ Aliases.ps1
@@ -0,0 +1,10 @@
+﻿function abcfunc {  
+    docker run -i --rm -v abc:/root appbaseio/abc
+}
+Set-Alias -Name abc -Value abcfunc
+
+function elasticfunc {
+	docker run -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:7.8.1
+}
+
+Set-Alias -Name elastic -Value elasticfunc
